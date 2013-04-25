2d432ab5a505746058789515806b771d7c8d0a79
Eric Bréchemier <eric@bezen.org>
2013-04-25 12:16:36 +0200

    Add remove() function to remove all items with given value from an array

---
+++ remove.js
@@ -0,0 +1,15 @@
+/*
+  Remove all items with given value from given array
+
+  Parameters:
+    array - array, the array to modify
+    value - any, the value to remove from the array (including duplicates)
+*/
+function remove( array, value ) {
+  var i;
+  for (i = array.length; i >= 0; i--) {
+    if ( array[i] === value ){
+      array.splice( i, 1 );
+    }
+  }
+}
