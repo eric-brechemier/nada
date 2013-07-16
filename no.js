bcc22c386d242e38e2ebfc5e545650742893646c
Eric Bréchemier <eric@bezen.org>
2013-07-16 17:25:16 +0200

    Complete documentation of function no()

--- no.js
+++ no.js
@@ -1,5 +1,12 @@
 /*
   Check whether given value is null or undefined
+
+  Parameter:
+    value - any, the value to check
+
+  Returns:
+    boolean, false when the value is null or undefined,
+    false otherwise
 */
 function no( value ) {
   var undef; // do not trust global undefined, which can be set to a value
