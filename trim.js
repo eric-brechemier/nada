ff7d0f9ec354b67f699819a8f2476d7dfd2a4798
Eric Bréchemier <eric@bezen.org>
2013-07-16 18:21:49 +0200

    Simplify implementation of trim()
    This implementation is based on the shim for String.prototype.trim
    described in [1].
    
    Instead of extracting the expected substring from the string,
    the replace function now removes whitespace from the start and
    end of the string.
    
    Reference:
    [1] Section: Compatibility
    in String.trim
    in Mozilla Developer Network
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Compatibility

--- trim.js
+++ trim.js
@@ -12,23 +12,11 @@ function trim( string ) {
   // Details of the regular expression used below:
   // Replace any substring matching:
   //   ^ the start of the string
-  //   \s* optionally followed with one or several white-space characters
-  //   ( followed with group 1:
-  //     [^]*? optionally one or several of any character including newlines,
-  //           in a non-greedy way (matching as little characters as possible)
-  //   ) end of group 1
-  //   \s* optionally followed with one or several white-space characters
+  //   \s+ followed with one or several white-space characters
+  //   | or
+  //   \s+ one or several white-space characters
   //   $ followed with the end of the string
   // with:
-  //   $1 the group 1
-  //
-  // Note:
-  // /./ matches any character except newline characters.
-  // /[^]/ matches any character including newline characters.
-  //
-  // Reference:
-  // Special Characters in Regular Expressions
-  // in Mozilla Developer Network
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#Special_characters_in_regular_expressions
-  return string.replace( /^\s*([^]*?)\s*$/, "$1" );
+  //   an empty string
+  return string.replace( /^\s+|\s+$/g, "" );
 }
