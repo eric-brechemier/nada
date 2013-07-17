d32a49ef7d181dc0919c7a6cc02aa06aac01fa2f
Eric Bréchemier <eric@bezen.org>
2013-07-17 10:41:07 +0200

    Add substringAfter() to extract the end of a string after given substring
    The unit tests are adapted from unit tests of contains() in the same project.

---
+++ substringAfter.js
@@ -0,0 +1,18 @@
+/*
+  Extract the end of a string after given substring
+
+  Parameters:
+    string - string, the string from which to extract the end
+    substring - string, the substring to look for
+
+  Returns:
+    string, the end of the string after the first occurrence of given substring
+    or the empty string when the substring is not found in the string.
+*/
+function substringAfter( string, substring ) {
+  var position = string.indexOf( substring );
+  if ( position === -1 ) {
+    return string;
+  }
+  return string.slice( position + substring.length );
+}
