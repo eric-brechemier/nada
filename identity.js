fb80de4b1b5852a58ebf88d3aca3fc47655a42bc
Eric Bréchemier <eric@bezen.org>
2014-04-16 16:14:49 +0200

    Add identity() function which returns argument unchanged

---
+++ identity.js
@@ -0,0 +1,12 @@
+/*
+  Identity Function: return the given argument
+
+  Parameter:
+    value - any value
+
+  Returns:
+    the same value provided as parameter
+*/
+function identity( value ) {
+  return value;
+}
